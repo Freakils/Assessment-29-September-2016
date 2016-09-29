549b29caa59d1b7b2a2f75baf4897d487a2146ec
Freakils <luvo@projectcodex.co>
2016-09-29 13:26:47 +0200

    changes

--- mapping-the-bill.js
+++ mapping-the-bill.js
@@ -35,68 +35,13 @@ exports.mappingTheBill = function(filepath){
             newBillArray.forEach(function(billList){
               var provider = billList.provider;
               var cell = billList.cellNum;
-              var duration = billList.duration;
 
-                // if(billMap[cell] === undefined){
-                //   billMap[cell] = 0;
-                // }
-                //
-                // billMap[cell] += provider;
+              billMap[cell] = provider;
+
+            // console.log(cell);
             });
-
+            
             console.log(billMap);
             return billMap;
 
 };
-
-// var productMap = {};
-//
-// dataToArray.forEach(function(list) {
-//     var item = list.products;
-//     var qty = list.sold;
-//
-//     if (productMap[item] === undefined) {
-//       productMap[item] = 0;
-//       }
-//
-//     productMap[item] += qty;
-//   });
-//
-//   // console.log(productMap);
-//   return productMap;
-// };
-
-// // creating an array.
-// exports.mappedWeeks = function(filepath) {
-//
-//   var list = fs.readFileSync(filepath, "utf8");
-//
-//   var newList = list
-//     .split('\n')
-//     .splice(1)
-//     .filter(Boolean);
-//
-//     // console.log(newList);
-//
-//
-//   var dataToArray = [];
-//
-//   for (var i = 0; i < newList.length; i++) {
-//     newList[i] = newList[i]
-//       .split(',')
-//       .splice(1);
-//
-//
-//     dataToArray.push({
-//
-//       date: newList[i][0] + '-2016',
-//       products: newList[i][1],
-//       sold: Number(newList[i][2]),
-//       price: Number(newList[i][3].replace(/R/, ''))
-//
-//     })
-// }
-//   // console.log(dataToArray);
-//
-//
-//
--- test/assesment_test.js
+++ test/assesment_test.js
@@ -2,11 +2,28 @@ var assert = require("assert");
 
 var mappingTheBill = require('../mapping-the-bill')
 
-describe('mapping the bill', function(){
+describe('mapping the bill', function() {
 
-  it('shoul return a map of the bill', function(){
-      var result = {};
-    assert.deepEqual(result,mappingTheBill.mappingTheBill('./data-file/ItemisedBill.csv'));
-    // assert.deepEqual(weekOne, mappingTheWeeks.mappedWeeks("./data-files/week1.csv"));
+  it('shoul return a map of the bill', function() {
+    var billMap = {
+      '0832401145': 'MTN',
+      '0838758090': 'MTN',
+      '0821302398': 'Vodacom',
+      '0841257809': 'CellC',
+      '0828907600': 'Vodacom',
+      '0825605600': 'CellC',
+      '0824501276': 'Vodacom',
+      '0821005078': 'Vodacom',
+      '0831239023': 'MTN',
+      '0845009087': 'CellC',
+      '0828009712': 'Vodacom',
+      '0832004576': 'MTN',
+
+      '0837351200': 'MTN',
+      '0828901271': 'Vodacom',
+      '0834590001': 'MTN',
+      '0824009001': 'Vodacom'
+    };
+    assert.deepEqual(billMap, mappingTheBill.mappingTheBill('./data-file/ItemisedBill.csv'));
   })
 });
