7a017f922ae3344c3d15c2fe35b79c3cbba939b5
Freakils <luvo@projectcodex.co>
2016-09-29 11:24:55 +0200

    changes

--- mapping-the-bill.js
+++ mapping-the-bill.js
@@ -1,3 +1,84 @@
 var fs = require("fs");
 
-exports.mappingTheBill = function(){};
+// creatin the array.
+exports.mappingTheBill = function(filepath){
+
+  var billcsv = fs.readFileSync(filepath, 'utf8');
+    // console.log(billcsv);
+
+      var newBill = billcsv
+          .split('\r')
+          .splice(1)
+          .filter(Boolean);
+
+            // console.log(newBill);
+
+          var newBillArray = [];
+
+            for(var i = 0; i < newBill.length; i++){
+              newBill[i] = newBill[i]
+                .split(',')
+                // .splice(1)
+
+                newBillArray.push({
+                  date: newBill[i][0] + '-2015',
+                  provider: newBill[i][1],
+                  cellNum: Number(newBill[i][2]),
+                  duration: Number()
+                })
+            }
+
+            // console.log(newBillArray);
+
+
+
+
+};
+// // creating an array.
+// exports.mappedWeeks = function(filepath) {
+//
+//   var list = fs.readFileSync(filepath, "utf8");
+//
+//   var newList = list
+//     .split('\n')
+//     .splice(1)
+//     .filter(Boolean);
+//
+//     // console.log(newList);
+//
+//
+//   var dataToArray = [];
+//
+//   for (var i = 0; i < newList.length; i++) {
+//     newList[i] = newList[i]
+//       .split(',')
+//       .splice(1);
+//
+//
+//     dataToArray.push({
+//
+//       date: newList[i][0] + '-2016',
+//       products: newList[i][1],
+//       sold: Number(newList[i][2]),
+//       price: Number(newList[i][3].replace(/R/, ''))
+//
+//     })
+// }
+//   // console.log(dataToArray);
+//
+//   var productMap = {};
+//
+//   dataToArray.forEach(function(list) {
+//     var item = list.products;
+//     var qty = list.sold;
+//
+//     if (productMap[item] === undefined) {
+//       productMap[item] = 0;
+//       }
+//
+//     productMap[item] += qty;
+//   });
+//
+//   // console.log(productMap);
+//   return productMap;
+// };
--- test/assesment_test.js
+++ test/assesment_test.js
@@ -5,6 +5,8 @@ var mappingTheBill = require('../mapping-the-bill')
 describe('mapping the bill', function(){
 
   it('shoul return a map of the bill', function(){
-    assert.equal();
+      var result = {};
+    assert.equal(result,mappingTheBill.mappingTheBill('./data-file/ItemisedBill.csv'));
+    // assert.deepEqual(weekOne, mappingTheWeeks.mappedWeeks("./data-files/week1.csv"));
   })
 });
