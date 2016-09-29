fa1c54c6d38a97baf7db79567ced047b510d5b2f
Freakils <luvo@projectcodex.co>
2016-09-29 10:16:18 +0200

    first commit

---
+++ test/assesment_test.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+
+var mappingTheBill = require('../mapping-the-bill')
+
+describe('mapping the bill', function(){
+
+  it('shoul return a map of the bill', function(){
+    assert.equal();
+  })
+});
