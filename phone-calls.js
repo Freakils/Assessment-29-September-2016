8192ea1977ca52dd2c98d808f87c01bf4edfb214
Freakils <luvo@projectcodex.co>
2016-09-30 10:51:00 +0200

    changes

--- phone-calls.js
+++ phone-calls.js
@@ -46,24 +46,3 @@ exports.ascendingByDuration = function(billObject){
     billObject.forEach(){}
 
 }
-
-// module.exports = function(list){
-//
-//   var myArray = [];
-//   for (var key in list) {
-//     if (list[key].hasOwnProperty("apples")) {
-//       myArray.push({
-//         shop: key,
-//         price: list[key].apples
-//       });
-//     }
-//   }
-// // console.log(myArray)
-//
-//   var myApple = myArray.slice(0);
-//      myApple.sort(function(a,b) {
-//       return a.price - b.price;
-//   });
-//     // console.log(myApple);
-//     return myApple;
-// }
